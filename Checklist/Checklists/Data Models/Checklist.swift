49c1cb675c603f6b598b0b64e3c2966a10740c7b
Ahmed Mohamed Essmat <49377210+ahmedessmat5254@users.noreply.github.com>
2021-03-02 13:50:33 +0200

    Add files via upload

---
+++ Checklist/Checklists/Data Models/Checklist.swift
@@ -0,0 +1,24 @@
+
+
+import UIKit
+
+class Checklist: NSObject, Codable {
+  var name = ""
+  var items = [ChecklistItem]()
+  var iconName = "No Icon"
+  
+  
+  init(name: String, iconName: String = "NoIcon") {
+    self.name = name
+    self.iconName = iconName
+    super.init()
+  }
+  
+  func countUncheckedItems() -> Int {
+    var count = 0
+    for item in items where !item.checked {
+      count += 1
+    }
+    return count
+  }
+}
